4914a65fc9cad9ecd534be4ef92ca22e463729b9
Daniel Hernando Miranda Tellez <danielh1991@hotmail.com>
2024-10-05 12:28:52 -0500

    Se agregan metatags

--- src/app/pages/pokemon-page/pokemon-page.component.ts
+++ src/app/pages/pokemon-page/pokemon-page.component.ts
@@ -8,6 +8,8 @@ import {
 import { Pokemon } from '../../pokemons/interfaces/pokemon.interface';
 import { PokemonService } from '../../pokemons/services/pokemon.service';
 import { ActivatedRoute } from '@angular/router';
+import { tap } from 'rxjs';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'pokemon-page',
@@ -20,16 +22,42 @@ import { ActivatedRoute } from '@angular/router';
 export default class PokemonPageComponent implements OnInit {
   private pokemonService = inject(PokemonService);
   private activatedRouter = inject(ActivatedRoute);
+  private title = inject(Title);
+  private meta = inject(Meta);
   public pokemon = signal<Pokemon | null>(null);
   ngOnInit(): void {
     const id = this.activatedRouter.snapshot.paramMap.get('id') ?? '';
-    this.pokemonService.loadPokemon(id).subscribe({
-      next: (pokemon) => {
-        this.pokemon.set(pokemon);
-      },
-      error: (error) => {
-        console.error(error);
-      },
-    });
+    this.pokemonService
+      .loadPokemon(id)
+      .pipe(
+        tap(({ name, id }) => {
+          this.title.setTitle(`${name} - #${id}`);
+          this.meta.updateTag({
+            name: 'description',
+            content: `Pokemon ${name} - #${id}`,
+          });
+          this.meta.updateTag({
+            name: 'og:title',
+            content: `${name} - #${id}`,
+          });
+          this.meta.updateTag({
+            name: 'og:description',
+            content: `Pokemon ${name} - #${id}`,
+          });
+          this.meta.updateTag({ name: 'keywords', content: `${name}, ${id}` });
+          this.meta.updateTag({
+            name: 'og:image',
+            content: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`,
+          });
+        })
+      )
+      .subscribe({
+        next: (pokemon) => {
+          this.pokemon.set(pokemon);
+        },
+        error: (error) => {
+          console.error(error);
+        },
+      });
   }
 }
